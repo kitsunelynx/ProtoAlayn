8e9a8ddc871f33893b888a9061cd0df85a19570e
kitsunelynx <kitsunelynx2@gmail.com>
2024-12-30 14:16:41 +0530

    Update font

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,10 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Baskervville } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const baskervville = Baskervville({ 
+  weight: '400',
+  subsets: ['latin'],
+  variable: '--font-baskervville',
 });
 
 export const metadata = {
@@ -19,9 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${baskervville.variable} font-serif`}>
         {children}
       </body>
     </html>
